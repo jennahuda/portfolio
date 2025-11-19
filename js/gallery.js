c9a86bc3850bc20b2fc9fd2354182cbb0bb2df1a
Huda Khan <hudakhan@Hudas-MacBook-Pro.local>
2025-11-19 19:53:52 +0000

    Added photos

--- js/gallery.js
+++ js/gallery.js
@@ -15,3 +15,40 @@ buttons.forEach(btn => {
     });
   });
 });
+
+
+const galleries = {
+    portraits: ["images/portraits/p1.jpg", "images/portraits/p2.jpg"],
+    travel: ["images/travel/t1.jpg", "images/travel/t2.jpg"],
+    wildlife: ["images/wildlife/w1.jpg", "images/wildlife/w2.jpg"]
+  };
+  
+  let currentGenre = "portraits";
+  let currentIndex = 0;
+  
+  function showGenre(genre) {
+    currentGenre = genre;
+    currentIndex = 0;
+    updateCarousel();
+  }
+  
+  function updateCarousel() {
+    const carousel = document.getElementById("carousel");
+    carousel.innerHTML = `<img src="${galleries[currentGenre][currentIndex]}" alt="">`;
+  }
+  
+  function nextImage() {
+    currentIndex++;
+    if (currentIndex >= galleries[currentGenre].length) currentIndex = 0;
+    updateCarousel();
+  }
+  
+  function prevImage() {
+    currentIndex--;
+    if (currentIndex < 0) currentIndex = galleries[currentGenre].length - 1;
+    updateCarousel();
+  }
+  
+  // Initialize carousel
+  updateCarousel();
+  
